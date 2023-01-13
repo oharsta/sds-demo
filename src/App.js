1f62227e9a1207363a7b7e31a9d4f8c83a045c4e
Okke Harsta <oharsta@zilverline.com>
2023-01-13 12:47:25 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Pagination} from "@surfnet/sds";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [nbr, setNbr] = useState(1);
+
+    return (
+        <div className="main-app sds--color-palette--blue">
+            <h1>Is this Nunito</h1>
+            <Button onClick={() => alert('Hi')} txt={"Does this work?"}/>
+            <Pagination onChange={newNbr => setNbr(newNbr)} currentPage={nbr} total={120}/>
+        </div>
+    );
 }
 
 export default App;
